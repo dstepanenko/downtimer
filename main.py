97667aafe6477671effd664b56ddbafcd9f984cb
Dmitry Stepanenko <dstepanenko@mirantis.com>
2016-08-03 17:22:54 +0200

    Added report_file option into conf.ini.sample config Now report is written into file specified in report_file option of config file

--- main.py
+++ main.py
@@ -48,6 +48,7 @@ class Config:
         self.auth_url = conf.get('global', 'keystone_endpoint')
         self.os_user = conf.get('global', 'user')
         self.os_pass = conf.get('global', 'password')
+        self.report_file = conf.get('global', 'report_file')
 
         self.db_host = conf.get('db', 'host')
         self.db_port = conf.get('db', 'port')
@@ -112,61 +113,63 @@ class Downtimer:
                                'status_code <> 200 and status_code <> 300 '
                                'group by service_name;')
 
-        for service in service_to_track:
-            key = ('service_response', {'service_name': service})
-            try:
-                value = total_srv[key].next()
-                total_uptime = value['count']
-            except:
-                print "There's no records for service", service
-                continue
+        with open(self.conf.report_file, "w") as f:
+            for service in service_to_track:
+                key = ('service_response', {'service_name': service})
+                try:
+                    value = total_srv[key].next()
+                    total_uptime = value['count']
+                except:
+                    f.write("There's no records for service %s\n" % service)
+                    continue
 
-            try:
-                value = bad_srv[key].next()
-                srv_downtime = value['count']
-            except:
-                srv_downtime = 0
+                try:
+                    value = bad_srv[key].next()
+                    srv_downtime = value['count']
+                except:
+                    srv_downtime = 0
 
-            print ("Service %s was down approximately %d seconds which are %.1f"
-                   "%% of total uptime" %
-                   (service, srv_downtime, (100.0 * srv_downtime) / total_uptime))
+                f.write("Service %s was down approximately %d seconds which is"
+                        " %.1f%% of total uptime\n" %
+                        (service, srv_downtime,
+                        (100.0 * srv_downtime) / total_uptime))
 
-        tags_resp = client.query('show tag values from floating_ip_pings '
-                                 'with key=address;')
-        addresses = [item['value'] for item in tags_resp[(u'floating_ip_pings', None)]]
-        total_ping = client.query('select count(value) from floating_ip_pings '
-                                  'group by address;')
-        bad_ping_exit_code = client.query('select count(value) from floating_ip_pings '
-                                          'where exit_code <> 0 group by address;')
-        partially_lost_ping = client.query('select sum(value) from floating_ip_pings '
-                                           'where exit_code = 0 group by address;')
+            tags_resp = client.query('show tag values from floating_ip_pings '
+                                     'with key=address;')
+            addresses = [item['value'] for item in tags_resp[(u'floating_ip_pings', None)]]
+            total_ping = client.query('select count(value) from floating_ip_pings '
+                                      'group by address;')
+            bad_ping_exit_code = client.query('select count(value) from floating_ip_pings '
+                                              'where exit_code <> 0 group by address;')
+            partially_lost_ping = client.query('select sum(value) from floating_ip_pings '
+                                               'where exit_code = 0 group by address;')
 
-        for address in addresses:
-            key = ('floating_ip_pings', {'address': address})
-            try:
-                value = total_ping[key].next()
-                total_time = value['count']
-            except:
-                print "There's no records about address", address
-                continue
+            for address in addresses:
+                key = ('floating_ip_pings', {'address': address})
+                try:
+                    value = total_ping[key].next()
+                    total_time = value['count']
+                except:
+                    f.write("There's no records about address %s\n" % address)
+                    continue
 
-            try:
-                value =  bad_ping_exit_code[key].next()
-                failed_ping = value['count']
-            except:
-                failed_ping = 0
+                try:
+                    value =  bad_ping_exit_code[key].next()
+                    failed_ping = value['count']
+                except:
+                    failed_ping = 0
 
-            try:
-                value = partially_lost_ping[key].next()
-                lost_ping = value['sum'] * 0.05
-            except:
-                lost_ping = 0
+                try:
+                    value = partially_lost_ping[key].next()
+                    lost_ping = value['sum'] * 0.05
+                except:
+                    lost_ping = 0
 
-            failed = failed_ping + lost_ping
+                failed = failed_ping + lost_ping
 
-            print ("Address %s was unreachable approximately %.1f second which are %.1f"
-                   "%% of total uptime" %
-                    (address, failed, (100.0 * failed) / total_time))
+                f.write("Address %s was unreachable approximately %.1f second "
+                        "which is %.1f%% of total uptime\n" %
+                        (address, failed, (100.0 * failed) / total_time))
 
 
 def do_check(endpoint, address, db_url):
