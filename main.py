c74f44e6674fe6e080304263e06d6a3949ee64ad
Anton Grigoryev <agrigorev@mirantis.com>
2016-08-05 01:27:34 +0300

    [ICDE-1088] separate work with DB into class

--- main.py
+++ main.py
@@ -7,13 +7,14 @@ import time
 import threading
 
 from daemon import runner
-from datetime import datetime
-from influxdb import InfluxDBClient
 from keystoneauth1 import session
 from keystoneclient.v3 import client as keystone_client
 from keystoneauth1.identity import Password
 from neutronclient.v2_0 import client as neutron_client
+
+from datetime import datetime
 from urlparse import urlparse
+from db import InfluxDBAdapter
 
 SERVICE_TIMEOUT = 0.9
 CONFIG_FILE = "/etc/downtimer/conf.ini"
@@ -40,7 +41,7 @@ class Daemon(runner.DaemonRunner):
         }
 
 
-class Config:
+class Config(object):
     def __init__(self, file_name):
         conf = ConfigParser.ConfigParser()
         conf.read(CONFIG_FILE)
@@ -54,7 +55,7 @@ class Config:
         self.db_port = conf.get('db', 'port')
 
 
-class Downtimer:
+class Downtimer(object):
     def __init__(self, conf_file='conf.ini'):
         #  All these paths should be defined for using python-daemon runner
         self.stdin_path = '/dev/null'
@@ -63,10 +64,7 @@ class Downtimer:
         self.pidfile_path =  '/var/run/downtimer.pid'
         self.pidfile_timeout = 5
         self.conf = Config(conf_file)
-        self.db_url = 'http://{host}:{port}/write?db=endpoints'.format(
-            host=self.conf.db_host,
-            port=self.conf.db_port
-        )
+        self.db_adapter = InfluxDBAdapter(self.conf)
         self.threads = []
 
     def run(self):
@@ -80,12 +78,13 @@ class Downtimer:
                                                interface='public')
             url = urlparse(endpoint.url)
             new_url = "http://" + url.hostname + ":" + str(url.port) + "/"
-            self.add_worker(do_check, (service.name, new_url, self.db_url))
+            self.add_worker(do_check, (service.name, new_url, self.db_adapter))
 
         neutron = neutron_client.Client(session=sess)
         for fip in neutron.list_floatingips()['floatingips']:
             if fip['status'] == 'ACTIVE':
-                self.add_worker(ping, (fip['floating_ip_address'], self.db_url))
+                self.add_worker(ping, (fip['floating_ip_address'],
+                                       self.db_adapter))
 
         while True:
             time.sleep(3)
@@ -98,81 +97,24 @@ class Downtimer:
         self.threads.append(worker)
 
     def report(self):
-        #  TODO for 2 methods we're using different way to access influx db
-        #  this is not critical, because we're going to move to sql db soon
-        client = InfluxDBClient(self.conf.db_host, self.conf.db_port,
-                                database='endpoints')
-        #res = client.query('select count(value) from services;')
-        services_ref = client.query('show tag values from service_response '
-                                        'with key = service_name')
-        service_to_track = [x['value'] for x in services_ref[('service_response', None)]]
-
-        total_srv = client.query('select count(value) from service_response '
-                                 'group by service_name;')
-        bad_srv = client.query('select count(value) from service_response where '
-                               'status_code <> 200 and status_code <> 300 '
-                               'group by service_name;')
-
         with open(self.conf.report_file, "w") as f:
-            for service in service_to_track:
-                key = ('service_response', {'service_name': service})
-                try:
-                    value = total_srv[key].next()
-                    total_uptime = value['count']
-                except:
-                    f.write("There's no records for service %s\n" % service)
-                    continue
+            adapter = InfluxDBAdapter(self.conf)
 
-                try:
-                    value = bad_srv[key].next()
-                    srv_downtime = value['count']
-                except:
-                    srv_downtime = 0
+            for service in adapter.get_service_statuses():
+                f.write("Service %s was down approximately %d seconds which "
+                        "are %.1f%% of total uptime\n" %
+                        (service['service'], service['srv_downtime'],
+                         (100.0 * service['srv_downtime']) /
+                         service['total_uptime']))
 
-                f.write("Service %s was down approximately %d seconds which is"
-                        " %.1f%% of total uptime\n" %
-                        (service, srv_downtime,
-                        (100.0 * srv_downtime) / total_uptime))
-
-            tags_resp = client.query('show tag values from floating_ip_pings '
-                                     'with key=address;')
-            addresses = [item['value'] for item in tags_resp[(u'floating_ip_pings', None)]]
-            total_ping = client.query('select count(value) from floating_ip_pings '
-                                      'group by address;')
-            bad_ping_exit_code = client.query('select count(value) from floating_ip_pings '
-                                              'where exit_code <> 0 group by address;')
-            partially_lost_ping = client.query('select sum(value) from floating_ip_pings '
-                                               'where exit_code = 0 group by address;')
-
-            for address in addresses:
-                key = ('floating_ip_pings', {'address': address})
-                try:
-                    value = total_ping[key].next()
-                    total_time = value['count']
-                except:
-                    f.write("There's no records about address %s\n" % address)
-                    continue
-
-                try:
-                    value =  bad_ping_exit_code[key].next()
-                    failed_ping = value['count']
-                except:
-                    failed_ping = 0
-
-                try:
-                    value = partially_lost_ping[key].next()
-                    lost_ping = value['sum'] * 0.05
-                except:
-                    lost_ping = 0
-
-                failed = failed_ping + lost_ping
-
+            for address in adapter.get_instance_statuses():
                 f.write("Address %s was unreachable approximately %.1f second "
-                        "which is %.1f%% of total uptime\n" %
-                        (address, failed, (100.0 * failed) / total_time))
+                        "which are %.1f %% of total uptime\n" %
+                        (address['address'], address['failed'],
+                         (100.0 * address['failed']) / address['total_time']))
 
 
-def do_check(endpoint, address, db_url):
+def do_check(endpoint, address, db_adapter):
     while True:
         try:
             timeout = 0
@@ -188,18 +130,13 @@ def do_check(endpoint, address, db_url):
             wait_time = 1 - SERVICE_TIMEOUT
             print e
 
-        message = ('service_response,service_name=' + endpoint +
-            ',address=' + address +
-            ' status_code=' + str(r.status_code) + ',timeout=' +
-            str(timeout) + ',value=' + str(r.elapsed.microseconds))
-        influx_resp = requests.post(
-            db_url,
-            message)
-        print "Influx: " + str(influx_resp.status_code) + "\n"
+        db_adapter.store_service_status(endpoint, address, r.status_code,
+                                        timeout, r.elapsed.microseconds)
+
         time.sleep(wait_time)
     
     
-def ping(address, db_url):
+def ping(address, db_adapter):
     while True:
         try:
             response = subprocess.check_output(
@@ -217,12 +154,8 @@ def ping(address, db_url):
             packet_loss = '100'
             total_time = '2000'
         print 'ttt = %s loss = %s code = %s' % (total_time, packet_loss, exit_code)
-        message = ('floating_ip_pings,address=' + address + ' total_time=' + total_time +
-                   ',exit_code=' + exit_code + ',value=' + packet_loss)
-        influx_resp = requests.post(
-            db_url,
-            message)
-        print "Influx: " + str(influx_resp.status_code) + "\n"
+
+        db_adapter.store_instance_status(address, total_time, exit_code, packet_loss)
 
 
 logger = logging.getLogger("Downtimer")
