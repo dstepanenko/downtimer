d32d79d6fd9705f446ea18bb75fb705ce80fd475
Vladislav Kuzmin <vladiskuz@gmail.com>
2016-12-05 14:04:05 +0300

    Refactor codebase
    - add missing requirements
    - split main.py to modulles
    - change pythion-daemon to daemonize
    - add static mode

---
+++ downtimer/config.py
@@ -0,0 +1,54 @@
+import ConfigParser
+import socket
+
+CONFIG_FILE = "/etc/downtimer/conf.ini"
+
+
+class Config(object):
+    def __init__(self, file_name):
+        conf = ConfigParser.SafeConfigParser()
+        conf.read(file_name)
+
+        self.log_level = conf.get('DEFAULT', 'log_level')
+        self.log_file = conf.get('DEFAULT', 'log_file')
+        self.pid_file = conf.get('DEFAULT', 'pid_file')
+        self.log_format = conf.get('DEFAULT', 'log_format')
+        self.report_file = conf.get('DEFAULT', 'report_file')
+
+        self.mode = conf.get('global', 'mode')
+
+        if self.mode == 'static':
+            ips_str = conf.get('static', 'ips')
+            if not ips_str:
+                raise AttributeError('Option ips is epmty')
+
+            self.ips = ips_str.split(',')
+            for ip in self.ips:
+                try:
+                    socket.inet_aton(ip)
+                except socket.error:
+                    raise AttributeError('Illegal ip address %s passed '
+                                         'to ips option in config file' % ip)
+
+        elif self.mode == 'openstack':
+            self.auth_url = conf.get('openstack', 'endpoint')
+            self.user = conf.get('openstack', 'user')
+            self.password = conf.get('openstack', 'password')
+
+        self.db_adapter = conf.get('database', 'adapter')
+        self.db_host = conf.get('database', 'host')
+
+        if self.db_adapter == 'influx':
+            self.db_port = conf.get('influxdb', 'port')
+            try:
+                true_vars = [True, '1', 'True', 'true']
+                self.use_udp = conf.get('influxdb', 'use_udp') in true_vars
+            except Exception:
+                self.use_udp = False
+            try:
+                self.udp_port = int(conf.get('influxdb', 'udp_port'))
+            except Exception:
+                # 4444 is a default udp port for InfluxDBClient
+                self.udp_port = 4444
+
+CONF = Config(CONFIG_FILE)
